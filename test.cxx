f52d52935ccb174dcced0db48bef0828fd64e870
nkmathew <kipkoechmathew@gmail.com>
2014-07-14 17:35:35 +0300

    Test font methods

--- test.cxx
+++ test.cxx
@@ -13,6 +13,7 @@ struct MoveTurtle : GC::EventHandler {
     shape = id;
   }
   void handle(Mouse mouse) {
+    canv->maximize();
     canv->rectangle(canv->BBox(shape));
     if (mouse.delta() >= 0) {
       canv->moveShape(shape, x, y);
@@ -28,16 +29,18 @@ int main() {
   int turtle;
   canv.showConsole();
   canv.icon("src/logo.ico");
-  int text = canv.text(100, 300, "Bazinga!!");
-  canv.setFont(text, "bold strikeout underline", 30, "Consolas");
-  FontAttr font = canv.getFont(text);
-
+  canv.text(420, 130, "all she wrote");
+  canv.text(100, 130, "all she wrote");
+  int text = canv.text(250, 130, "all she wrote");
+  canv.penColor("text", "green");
+  canv.fillColor("text", "black");
+  canv.setFont("text", "bold");
   turtle = canv.polygon({{0, 16}, { -2, 14}, { -1, 10}, { -4, 7}, { -7, 9}, { -9, 8},
     { -6, 5}, { -7, 1}, { -5, -3}, { -8, -6}, { -6, -8}, { -4, -5}, {0, -7}, {4, -5},
     {6, -8}, {8, -6}, {5, -3}, {7, 1}, {6, 5}, {9, 8}, {7, 9}, {4, 7}, {1, 10}, {2, 14}
   });
   canv.rectangle(canv.BBox(turtle));
   canv.fillColor(turtle, "yellow");
-  canv.bind("<Mouse-1>", MoveTurtle(20, 30, turtle, &canv), text);
+  canv.bind("<Mouse-1>", MoveTurtle(20, 30, text, &canv), "text");
   return canv.loop();
 }
