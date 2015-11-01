e550fcf29812a482ad24b23bbfdc9fafd2198b28
nkmathew <kipkoechmathew@gmail.com>
2015-11-01 12:56:57 +0300

    Generate tags into the project root folder only
    Stopped using autocd in Vim such that the working folder becomes
    the project root folder across the whole session

--- tags.cmd
+++ tags.cmd
@@ -1,8 +1,2 @@
 @echo off
-pushd src
-ctags *.h *.cxx --language-force=c++ --c++-kinds=pf
-popd
-pushd examples
-ctags ..\src\*.h ..\src\*.cxx *.cxx --language-force=c++ --c++-kinds=pf
-popd
-
+ctags src/* examples/* --language-force=c++ --c++-kinds=pf
